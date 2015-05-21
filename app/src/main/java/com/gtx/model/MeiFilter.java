4e4f52662e9a85d9f16de606075409d64f09f7c6
gtx1049 <gtx_122@126.com>
2015-05-21 11:28:02 +0800

    保存基本信息与图片信息

--- app/src/main/java/com/gtx/model/MeiFilter.java
+++ app/src/main/java/com/gtx/model/MeiFilter.java
@@ -1,19 +1,23 @@
 package com.gtx.model;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import org.apache.http.Header;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * Created by Administrator on 2015/5/19.
  */
 public class MeiFilter extends BaseFilter
 {
-
+    public static String TAG = "MeiFilter";
 
     public MeiFilter(Context context)
     {
@@ -32,7 +36,8 @@ public class MeiFilter extends BaseFilter
             {
                 Toast.makeText(context, "Success!", Toast.LENGTH_SHORT);
                 String filename = "Mei.html";
-                writeToFile(filename, responseBody);
+                //writeToFile(filename, responseBody);
+                parseForm(new String(responseBody));
             }
 
             @Override
@@ -48,6 +53,35 @@ public class MeiFilter extends BaseFilter
                 // called before request is started
             }
         });
+        return null;
+    }
+
+    public Entry parseForm(String form)
+    {
+        Document doc = Jsoup.parse(form);
+
+        Element dl = doc.select("dl").first();
+
+        Element priceElement = dl.select("dd").first();
+        String price = priceElement.select("strong").first().html();
+
+        Element titleElement = priceElement.nextElementSibling();
+        String title = titleElement.select("h1").first().html();
+
+        String description = titleElement.select("p").first().html();
+
+        String address = doc.select("div.address").first().html();
+
+        String urlpic = doc.select("div.album").first().select("img").first().attr("src");
+
+        this.savePic(urlpic, urlpic.substring(urlpic.length() - 15, urlpic.length()));
+
+        Log.d(TAG, "Price : " + price);
+        Log.d(TAG, "Title : " + title);
+        Log.d(TAG, "Description : " + description);
+        Log.d(TAG, "Address : " + address);
+        Log.d(TAG, "Pic : " + urlpic);
+
         return null;
     }
 
